8ff5c027d19afd1033a188aca328c540a8445939
nayeem01 <nayeem.khan@enosisbd.com>
2025-01-02 22:40:11 +0600

    Initial commit

---
+++ src/layout/Footer.js
@@ -0,0 +1,70 @@
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Hidden from '@mui/material/Hidden';
+import Link from '@mui/material/Link';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material';
+
+const Footer = () => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Box
+        backgroundColor={theme.palette.background.default}
+        paddingTop='1px'
+        paddingBottom='1px'
+        // sticky footer - see four values below
+        position='fixed'
+        bottom='0'
+        left='0'
+        width='100%'
+      >
+        <Divider />
+        <Box
+          backgroundColor={theme.palette.background.default}
+          position='relative'
+          padding={theme.spacing(0.25)}
+        >
+          <Grid container spacing={0}>
+            <Hidden mdDown>
+              <Grid container item xs={12} md={4}>
+                <List
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    padding: 0,
+                  }}
+                >
+                </List>
+              </Grid>
+            </Hidden>
+            <Grid container item xs={12} md={4} justifyContent='center'>
+              <List>
+                <ListItemButton>
+                  <ListItemText
+                    primary={
+                      <Typography
+                        variant='body2'
+                        color={theme.palette.text.secondary}
+                      >
+                        Developed by : Tasmia Yemna Azim
+                      </Typography>
+                    }
+                  />
+                </ListItemButton>
+              </List>
+            </Grid>
+            
+          </Grid>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Footer;
